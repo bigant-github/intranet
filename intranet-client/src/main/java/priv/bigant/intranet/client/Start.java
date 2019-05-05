748cdb15507ac3fbf211b5ebd6f0d53cfc5e2d3d
yapeng <yapeng.gao@soucaigroup.com>
2019-05-05 11:40:19 +0800

    code style

--- intranet-client/src/main/java/priv/bigant/intranet/client/Start.java
+++ intranet-client/src/main/java/priv/bigant/intranet/client/Start.java
@@ -1,9 +1,11 @@
 package priv.bigant.intranet.client;
 
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -25,7 +27,8 @@ public class Start {
         //new Home().showHome();
         FileInputStream inputStream = null;
         try {
-            inputStream = new FileInputStream(Start.class.getResource("conf.properties").getPath());
+            inputStream = new FileInputStream(new File("/tmp/conf.properties"));
+            //inputStream = new FileInputStream(Start.class.getResource("conf.properties").getPath());
             //inputStream = new FileInputStream(new File(userPath + "/conf.properties"));
             Properties properties = new Properties();
             properties.load(inputStream);
