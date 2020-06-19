b1475097797417db2dceb8bd90af49eb455f5db1
yapeng <yapeng@soucaigroup.com>
2020-06-19 17:46:18 +0800

    优化代码

--- intranet-server/src/main/java/priv/bigant/intranet/server/process/HttpIntranetAcceptProcess.java
+++ intranet-server/src/main/java/priv/bigant/intranet/server/process/HttpIntranetAcceptProcess.java
@@ -1,18 +1,15 @@
 package priv.bigant.intranet.server.process;
 
-import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import priv.bigant.intrance.common.*;
 import priv.bigant.intrance.common.communication.Communication;
 import priv.bigant.intrance.common.communication.CommunicationRequest;
 import priv.bigant.intrance.common.communication.CommunicationRequest.CommunicationRequestHttpAdd;
-import priv.bigant.intrance.common.communication.CommunicationResponse;
 import priv.bigant.intrance.common.communication.HttpCommunication;
 import priv.bigant.intranet.server.ServerConfig;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
@@ -65,67 +62,8 @@ public class HttpIntranetAcceptProcess extends ProcessBase {
     @Override
     public void accept(ServerConnector.ConnectorThread connectorThread, SelectionKey selectionKey) throws IOException {
         SocketChannel socketChannel = ((ServerSocketChannel) selectionKey.channel()).accept();
-
         socketChannel.configureBlocking(false);
         connectorThread.register(socketChannel, SelectionKey.OP_READ);
-
     }
-
-    class ReadProcessThread implements Runnable {
-
-        private SocketChannel socketChannel;
-        private SocketBean socketBean;
-
-        public ReadProcessThread(SocketChannel socketChannel) throws IOException {
-            this.socketChannel = socketChannel;
-            this.socketBean = new SocketBean(socketChannel);
-        }
-
-
-        @Override
-        public void run() {
-            try {
-                ByteBuffer allocate = ByteBuffer.allocate(1024);
-                int read = socketChannel.read(allocate);
-                allocate.flip();
-                CommunicationRequestHttpAdd communicationRequestHttpAdd = CommunicationResponse.createCommunicationResponse(ArrayUtils.subarray(allocate.array(), 0, read)).toJavaObject(CommunicationRequestHttpAdd.class);
-                String id = communicationRequestHttpAdd.getId();
-                socketBean.setId(id);
-                HttpCommunication serverCommunication = HttpSocketManager.get(HttpSocketManager.getKey(id));
-                serverCommunication.putSocketBean(socketBean);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    /*class ReadProcessThread implements Runnable {
-
-        private SocketChannel socketChannel;
-        private SocketBean socketBean;
-
-        public ReadProcessThread(SocketChannel socketChannel) throws IOException {
-            this.socketChannel = socketChannel;
-            this.socketBean = new SocketBean(socketChannel);
-        }
-
-
-
-        @Override
-        public void run() {
-            try {
-                ByteBuffer allocate = ByteBuffer.allocate(1024);
-                int read = socketChannel.read(allocate);
-                allocate.flip();
-                CommunicationRequest.CommunicationRequestHttpAdd communicationRequestHttpAdd = CommunicationResponse.createCommunicationResponse(ArrayUtils.subarray(allocate.array(), 0, read)).toJavaObject(CommunicationRequest.CommunicationRequestHttpAdd.class);
-                String id = communicationRequestHttpAdd.getId();
-                socketBean.setId(id);
-                HttpCommunication serverCommunication = HttpSocketManager.get(HttpSocketManager.getKey(id));
-                serverCommunication.putSocketBean(socketBean);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }*/
 
 }
